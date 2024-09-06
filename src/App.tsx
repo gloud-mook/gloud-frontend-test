939c7a6b047b0afbce97b1198a0b195a1be688cb
gloud-mook <dotorimook@gloud.io>
2024-09-06 09:40:33 +0900

    install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import "./index.css";
+
 function App() {
   return (
     <>
-      <div>
+      <div className="text-3xl font-bold underline bg-purple-500">
         <h1>Hello World!</h1>
       </div>
     </>
