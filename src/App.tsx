4ddab6888f4e0fdababba6441b5470bd541408dd
gloud-mook <dotorimook@gloud.io>
2024-09-06 10:17:14 +0900

    test firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig.ts";
 import "./index.css";
 
 function App() {
+  const handleFetchData = async () => {
+    const docRef = doc(db, "test", "aaaa");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("docSnap data : ", docSnap.data());
+    } else {
+      alert("Error");
+    }
+
+    console.log("click!!");
+  };
   return (
     <>
-      <div className="text-3xl font-bold underline bg-purple-500">
-        <h1>Hello World!</h1>
+      <div className="">
+        <button className="text-purple-700 text-3xl font-bold" onClick={handleFetchData}>
+          Click me! 😄
+        </button>
       </div>
     </>
   );
